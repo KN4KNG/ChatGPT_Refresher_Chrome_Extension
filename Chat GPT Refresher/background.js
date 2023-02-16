fd14702260609635fd1ba8c791bdcfae9ed64df8
Alex Bowman <KN4KNG@outlook.com>
2023-02-16 10:41:58 -0500

    Create background.js

---
+++ Chat GPT Refresher/background.js
@@ -0,0 +1,74 @@
+let refreshCount = 0;
+let refreshTimer = null;
+
+function checkPage(tabId, tabUrl) {
+  if (tabUrl.includes("https://www.chatgpt.com/login")) {
+    chrome.browserAction.setBadgeText({ text: "" });
+    return;
+  }
+
+  chrome.tabs.executeScript(tabId, { code: "document.body.innerText" }, function (result) {
+    const bodyText = result[0].toLowerCase();
+    if (bodyText.includes("chatgpt is at capacity right now")) {
+      refreshCount++;
+      chrome.browserAction.setBadgeText({ text: refreshCount.toString() });
+      if (refreshCount === 1) {
+        refreshTimer = setTimeout(refreshPage, 300000); // 5 minutes
+      }
+    } else {
+      refreshCount = 0;
+      chrome.browserAction.setBadgeText({ text: "" });
+    }
+  });
+}
+
+function refreshPage(tabId) {
+  chrome.tabs.reload(tabId);
+  refreshTimer = setTimeout(refreshPage, 5000, tabId);
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete") {
+    checkPage(tabId, tab.url);
+  }
+});
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  const tabUrl = tab.url.toLowerCase();
+  if (tabUrl.includes("https://www.chatgpt.com/login")) {
+    return;
+  }
+
+  if (refreshTimer) {
+    clearTimeout(refreshTimer);
+    refreshTimer = null;
+    refreshCount = 0;
+    chrome.browserAction.setBadgeText({ text: "" });
+  } else {
+    checkPage(tab.id, tabUrl);
+  }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request === "confirmRefresh") {
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "icon.png",
+      title: "ChatGPT Refresher",
+      message: "ChatGPT is still at capacity. Do you want to continue refreshing?",
+      buttons: [
+        { title: "Yes" },
+        { title: "No" }
+      ]
+    }, function (notificationId) {
+      chrome.notifications.onButtonClicked.addListener(function (buttonNotificationId, buttonIndex) {
+        if (buttonNotificationId === notificationId) {
+          if (buttonIndex === 0) {
+            refreshPage(sender.tab.id);
+          }
+          chrome.notifications.clear(notificationId);
+        }
+      });
+    });
+  }
+});
