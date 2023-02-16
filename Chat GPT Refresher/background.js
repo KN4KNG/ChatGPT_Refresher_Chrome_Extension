467e1a288803072782361aea04eee455714224be
Alex Bowman <KN4KNG@outlook.com>
2023-02-16 11:32:20 -0500

    Add files via upload

--- Chat GPT Refresher/background.js
+++ Chat GPT Refresher/background.js
@@ -3,23 +3,38 @@ let refreshTimer = null;
 
 function checkPage(tabId, tabUrl) {
   if (tabUrl.includes("https://www.chatgpt.com/login")) {
-    chrome.browserAction.setBadgeText({ text: "" });
+    chrome.action.setBadgeText({ text: "" });
     return;
   }
 
-  chrome.tabs.executeScript(tabId, { code: "document.body.innerText" }, function (result) {
-    const bodyText = result[0].toLowerCase();
-    if (bodyText.includes("chatgpt is at capacity right now")) {
-      refreshCount++;
-      chrome.browserAction.setBadgeText({ text: refreshCount.toString() });
-      if (refreshCount === 1) {
-        refreshTimer = setTimeout(refreshPage, 300000); // 5 minutes
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tabId },
+      func: () => document.body.innerText,
+    },
+    (result) => {
+      const bodyText = result[0].result.toLowerCase();
+      if (bodyText.includes("chatgpt is at capacity right now")) {
+        refreshCount++;
+        chrome.action.setBadgeText({ text: refreshCount.toString() });
+        if (refreshCount === 1) {
+          refreshTimer = setTimeout(refreshPage, 300000); // 5 minutes
+        }
+      } else {
+        refreshCount = 0;
+        chrome.action.setBadgeText({ text: "" });
+        chrome.notifications.create(
+          {
+            type: "basic",
+            iconUrl: "/images/icon.png",
+            title: "ChatGPT Refresher",
+            message: "ChatGPT is ready",
+          },
+          function () {}
+        );
       }
-    } else {
-      refreshCount = 0;
-      chrome.browserAction.setBadgeText({ text: "" });
     }
-  });
+  );
 }
 
 function refreshPage(tabId) {
@@ -33,7 +48,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   }
 });
 
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
   const tabUrl = tab.url.toLowerCase();
   if (tabUrl.includes("https://www.chatgpt.com/login")) {
     return;
@@ -43,7 +58,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     clearTimeout(refreshTimer);
     refreshTimer = null;
     refreshCount = 0;
-    chrome.browserAction.setBadgeText({ text: "" });
+    chrome.action.setBadgeText({ text: "" });
   } else {
     checkPage(tab.id, tabUrl);
   }
@@ -51,24 +66,27 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request === "confirmRefresh") {
-    chrome.notifications.create({
-      type: "basic",
-      iconUrl: "icon.png",
-      title: "ChatGPT Refresher",
-      message: "ChatGPT is still at capacity. Do you want to continue refreshing?",
-      buttons: [
-        { title: "Yes" },
-        { title: "No" }
-      ]
-    }, function (notificationId) {
-      chrome.notifications.onButtonClicked.addListener(function (buttonNotificationId, buttonIndex) {
-        if (buttonNotificationId === notificationId) {
-          if (buttonIndex === 0) {
-            refreshPage(sender.tab.id);
+    chrome.notifications.create(
+      {
+        type: "basic",
+        iconUrl: "/images/icon.png",
+        title: "ChatGPT Refresher",
+        message: "ChatGPT is still at capacity. Do you want to continue refreshing?",
+        buttons: [
+          { title: "Yes" },
+          { title: "No" }
+        ]
+      },
+      function (notificationId) {
+        chrome.notifications.onButtonClicked.addListener(function (buttonNotificationId, buttonIndex) {
+          if (buttonNotificationId === notificationId) {
+            if (buttonIndex === 0) {
+              refreshPage(sender.tab.id);
+            }
+            chrome.notifications.clear(notificationId);
           }
-          chrome.notifications.clear(notificationId);
-        }
-      });
-    });
+        });
+      }
+    );
   }
 });
